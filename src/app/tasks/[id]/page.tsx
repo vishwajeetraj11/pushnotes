5a007242fc196b7d8500a802864280ae21444b2b
Vishwajeet Raj <ext-vishwajeet.raj@graphy.com>
2023-10-10 19:13:09 +0530

    added nav and pages for task by id

--- src/app/tasks/[id]/page.tsx
+++ src/app/tasks/[id]/page.tsx
@@ -1,13 +1,35 @@
 import Nav from "@/components/layout/nav";
+import Image from "next/image";
 import React from "react";
 
 type Props = {};
 
 const TasksByIdPage = (props: Props) => {
+  const data = {
+    title: "Task Title",
+    image:
+      "https://images.unsplash.com/photo-1515847049296-a281d6401047?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80",
+    description: `A death march is a forced march of prisoners of war or other captives or deportees in which individuals are left to die along the way.[1] It is distinguished in this way from simple prisoner transport via foot march. Article 19 of the Geneva Convention requires that prisoners must be moved away from a danger zone such as an advancing front line, to a place that may be considered more secure. It is not required to evacuate prisoners that are too unwell or injured to move. In times of war such evacuations can be difficult to carry out.
+    Death marches usually feature harsh physical labour and abuse, neglect of prisoner injury and illness, deliberate starvation and dehydration, humiliation, torture, and execution of those unable to keep up the marching pace. The march may end at a prisoner-of-war camp or internment camp, or it may continue until all the prisoners are dead.`,
+  };
   return (
     <>
       <Nav title="Task" />
-      TasksByIdPage
+      <div className="px-10 mt-10">
+        <p>Task Title</p>
+        <div className="my-5">
+          <Image
+            priority
+            height={400}
+            width={400}
+            draggable={false}
+            src={data.image}
+            objectFit="contain"
+            alt={data.title + " Task Image"}
+          />
+        </div>
+        <p>{data.description}</p>
+      </div>
     </>
   );
 };
--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -12,12 +12,15 @@ import React from "react";
 type Props = { title: string };
 
 const Nav = ({ title }: Props) => {
+  const isTaskByIdPage = usePathname().includes("/tasks/[id]");
+
   return (
     <div className="flex justify-between flex-shrink-0 pr-6 border-b border-gray-200 py-3 h-22 lg:h-15 lg:py-5 pl-4 lg:pl-10">
       <div className="flex items-center flex-wrap">
         <div className="mt-2 lg:mt-0 text-sm font-semibold cursor-default">
           {title}
         </div>
+        {isTaskByIdPage ? "ss" : "ssa"}
       </div>
     </div>
   );
